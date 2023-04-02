73219a7e8c28fc97761b2a1970011d1d684cdf1f
slezkinis <slezkin.is@mail.ru>
2023-04-02 16:08:33 +0300

    Add annotate likes_count

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from blog.models import Comment, Post, Tag
+from django.db.models import Count
+
 
 
 def get_related_posts_count(tag):
@@ -28,11 +30,11 @@ def serialize_tag(tag):
 
 
 def get_likes_count(post):
-    return post.likes.count()
+    return post.likes_count
 
 
 def index(request):
-    all_posts = Post.objects.all()
+    all_posts = Post.objects.all().annotate(likes_count=Count('likes'))
     most_popular_posts = sorted(all_posts, key=get_likes_count, reverse=True)[:5]  # TODO. Как это посчитать?
 
     fresh_posts = Post.objects.order_by('published_at')
