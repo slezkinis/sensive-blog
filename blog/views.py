817d6c66374df87cde3f9da8c21cf01474afd4bc
slezkinis <slezkin.is@mail.ru>
2023-05-03 19:19:46 +0300

    Update getting comments

--- blog/views.py
+++ blog/views.py
@@ -53,7 +53,7 @@ def index(request):
 
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
-    comments = Comment.objects.filter(post=post).select_related('author')
+    comments = post.comments.all().select_related('author')
     serialized_comments = []
     for comment in comments:
         serialized_comments.append({
